72858c39f2108fd97c8880225275bf6817eeed14
mary <1093507006@qq.com>
2018-12-17 20:01:58 +0800

    varsion2.3

--- src/js/myadmin.js
+++ src/js/myadmin.js
@@ -1,47 +1,35 @@
 // 发送请求，请求管理员数据，渲染管理员列表
 let qty = 3;
 let now = 1;
-let page = (now - 1) * 2;
+let page = (now - 1) * 3;
 let total = 0;
 let num = 0;
 let totalPage = 0
 
-function require(qty, page) {
-	$.ajax({
-		type: 'get',
-		url: '/admin/check',
-		async: true,
-		data: {
-			'qty': qty,
-			'page': page
-		},
-		success: function(str) {
-			// 总页数
-			total = str.total;
-			totalPage = Math.ceil(total / qty);
-			arr = str.data;
-			create(arr);
-			createpage(total, now);
+// 发送ajax请求，渲染第一页
+$.ajax({
+	type: 'get',
+	url: '/admin/check',
+	async: true,
+	data: {
+		'qty': qty,
+		'page': page
+	},
+	success: function(str) {
+		// 总页数
+		total = str.total;
+		totalPage = Math.ceil(total / qty);
+		arr = str.data;
+		create(arr);
+
+		for(var i = 0; i < totalPage; i++) {
+			++num;
+			var str2 = `<a class="num" href="javascript:;">${num}</a>`;
+			$('#pagenum').html($('#pagenum').html() + str2);
 		}
-	});
-}
-
-require(qty, page);
-
-// 渲染页码
-function createpage(total, now) {
-	num = 0;
-	$('#pagenum').html('');
-	for(var i = 0; i < totalPage; i++) {
-		++num;
-		var str2 = `<a class="num" href="javascript:;">${num}</a>`;
-		$('#pagenum').html($('#pagenum').html() + str2);
+		updatePage();
 	}
-
-	var pageBtn = $('#pagenum').find('a');
-	pageBtn.eq(now - 1).addClass('current');
-	$('.totalPage').html(`共有数据：${total} 条`);
-}
+});
 
 // 渲染管理员列表
 // <input type="checkbox" name="pick" class="pick">
@@ -81,11 +69,40 @@ function create(arr) {
 	return $('#adminList').html(res);
 }
 
+// 更新页码状态
+function updatePage(){
+	var pageBtn = $('#pagenum').find('a');
+	pageBtn.removeClass('current');
+	pageBtn.eq(now - 1).addClass('current');
+	$('.totalPage').html(`共有数据：${total} 条`);
+}
+
+// 切换页码发送ajax请求
+function require(qty, page) {
+	$.ajax({
+		type: 'get',
+		url: '/admin/check',
+		async: true,
+		data: {
+			'qty': qty,
+			'page': page
+		},
+		success: function(str) {
+			// 总页数
+			total = str.total;
+			totalPage = Math.ceil(total / qty);
+			arr = str.data;
+			create(arr);
+			updatePage();
+		}
+	});
+}
+
 // 点击上下页
 //点击页码 
 $('#pagenum').on('click', '.num', function() {
 	now = $(this).text();
-	page = (now - 1) * 2;
+	page = (now - 1) * qty;
 	console.log(page);
 	require(qty, page);
 });
@@ -93,7 +110,7 @@ $('#pagenum').on('click', '.num', function() {
 // 上下页
 $('#prev').on('click', function() {
 	now--;
-	page = (now - 1) * 2;
+	page = (now - 1) * qty;
 	if(now <= 1) {
 		now = 1;
 		page = 0;
@@ -104,10 +121,10 @@ $('#prev').on('click', function() {
 
 $('#next').on('click', function() {
 	now++;
-	page = (now - 1) * 2;
+	page = (now - 1) * qty;
 	if(now >= totalPage) {
 		now = totalPage;
-		page = (totalPage - 1) * 2;
+		page = (totalPage - 1) * qty;
 	}
 	require(qty, page);
 });
@@ -116,10 +133,9 @@ $('#next').on('click', function() {
 var isok1 = false;
 $('#username').blur(() => {
 	var val = $('#username').val();
-	console.log(val);
 	var str = checkReg.trim(val); //去除前后空格
 
-	if(str && checkReg.username(str)) {
+	if(str) {
 		$.ajax({
 			type: 'get',
 			url: '/addAdmin/checkname',
@@ -134,8 +150,6 @@ $('#username').blur(() => {
 					$('#userInfor').css('color', 'red');
 					isok1 = false;
 				} else {
-					$('#userInfor').html('✔');
-					$('#userInfor').css('color', '#999');
 					isok1 = true;
 				}
 			}
@@ -147,145 +161,60 @@ $('#username').blur(() => {
 	}
 });
 
-// 验证手机号
-var isok2 = false;
-$('#phone').blur(() => {
-	var val = $('#phone').val();
-	var str = checkReg.trim(val);
-
-	if(str && checkReg.tel(str)) {
-		//非空且符合正则
-		$('#telInfor').html('✔');
-		$('#telInfor').css('color', '#999');
-		isok2 = true;
-	} else {
-		$('#telInfor').html('手机号不正确');
-		$('#telInfor').css('color', 'red');
-		isok2 = false;
-	}
-});
-
-// 验证邮箱
-var isok3 = false;
-$('#L_email').blur(() => {
-	var val = $('#L_email').val();
-	var str = checkReg.trim(val);
-
-	if(str && checkReg.email(str)) {
-		//非空且符合正则
-		$('#emailInfor').html('✔');
-		$('#emailInfor').css('color', '#999');
-		isok3 = true;
-	} else {
-		$('#emailInfor').html('邮箱格式不正确');
-		$('#emailInfor').css('color', 'red');
-		isok3 = false;
-	}
-});
-
-// 验证密码
-var isok4 = false;
-$('#L_pass').blur(() => {
-	var val = $('#L_pass').val();
-	var str = checkReg.trim(val);
-
-	if(str && checkReg.psw(str)) {
-		//非空且符合正则
-		$('#pswInfor').html('✔');
-		$('#pswInfor').css('color', '#999');
-		isok4 = true;
-	} else {
-		$('#pswInfor').html('密码不符合规则');
-		$('#pswInfor').css('color', 'red');
-		isok4 = false;
-	}
-});
-
-// 确认密码
-var isok5 = false;
-$('#L_repass').blur(function() {
-	var val = $('#L_repass').val();
-	var str = checkReg.trim(val);
-
-	if(str == $('#L_pass').val()) {
-		$('#conInfor').html('✔');
-		$('#conInfor').css('color', '#999');
-		isok5 = true;
-	} else {
-		$('#conInfor').html('两次输入密码不一致');
-		$('#conInfor').css('color', 'red');
-		isok5 = false;
-	}
-});
-
-var isok6 = false;
-var role = '';
+$('#role input').eq(0).prop('checked','checked');
+var role=$('#role input').eq(0).data('names');
 $('#role input').click(function() {
 	console.log($(this));
 	$(this).prop('checked', 'checked');
 	role = $(this).data('names');
-	console.log(role);
-
-	isok6 = true;
 });
 
-//点击添加管理员
-$('#addBtn').click(function() {
-	if(isok1 && isok2 && isok3 && isok4 && isok5 && isok6) {
-		$.ajax({
-			type: 'post',
-			url: '/addAdmin/addname',
-			async: true,
-			data: {
-				'username': $('#username').val(),
-				'password': $('#L_pass').val(),
-				'role': role,
-				'tel': $('#phone').val(),
-				'email': $('#L_email').val()
-			},
-			success: function(str) {
-				console.log(str);
-				// x_admin_show('','./admin-list.html');
+layui.use(['form', 'layer'], function() {
+    $ = layui.jquery;
+    var form = layui.form,
+        layer = layui.layer;
 
-			}
-		});
-	} else {
-		// alert('请输入完整信息');
-		console.log('请输入完整信息');
-	}
+    form.verify({
+	  username:[/^[a-zA-Z][\w\-]{2,19}$/,'登录名以字母开头，3-20位']
+	  ,pass: [/(.+){6,12}$/, '密码必须6到12位']
+	  ,repass: function(value){
+	      if($('#L_pass').val()!=$('#L_repass').val()){
+	          return '两次密码不一致';
+	      }
+	  }
+	});
+
+    // 监听提交
+    form.on('submit(add)', function() {
+        if (isok1) {
+            $.ajax({
+                type: 'post',
+                url: '/addAdmin/addname',
+                async: true,
+                data: {
+                    'username': $('#username').val(),
+                    'password': $('#L_pass').val(),
+                    'role': role,
+                    'tel': $('#phone').val(),
+                    'email': $('#L_email').val()
+                },
+                success: function(str) {
+                    console.log(str);
+                    layer.alert("增加成功", {
+				        icon: 6
+				    }, function() {
+				        // 获得frame索引
+				        var index = parent.layer.getFrameIndex(window.name);
+				        //关闭当前frame
+				        parent.layer.close(index);
+				    });
+				}
+            });
+        }else{
+        	return false
+        } 
+    });
 });
-
-// form.on('submit(add)', function(){
-//     // console.log(data);
-//     //发异步，把数据提交给php
-
-//     if(isok1 && isok2 && isok3 && isok4 && isok5 && isok6) {
-//         $.ajax({
-//             type: 'post',
-//             url: '/addAdmin/addname',
-//             async: true,
-//             data: {
-//                 'username': $('#username').val(),
-//                 'password': $('#L_pass').val(),
-//                 'role':role,
-//                 'tel':$('#phone').val(),
-//                 'email':$('#L_email').val()
-//             },
-//             success: function(str) {
-//                 console.log(str);
-//                 // x_admin_show('','./admin-list.html');
-//                 layer.alert("增加成功", {icon: 6},function () {
-//         // 获得frame索引
-//         var index = parent.layer.getFrameIndex(window.name);
-//         //关闭当前frame
-//         parent.layer.close(index);
-//     });
-//             }
-//         });
-//     }
-
-//     return false;
-// });
 
 // 点击编辑按钮
 // <a class="edit" title="编辑"  onclick="x_admin_show('编辑','admin-edit.html')" href="javascript:;">
@@ -322,20 +251,6 @@ $('#adminList').on('click', '.edit', function() {
 			var cookiestr = JSON.stringify(datalist);
 			console.log(cookiestr);
 			Cookie.set('data', cookiestr, {});
-
-			// $('#username2').val(data.username);
-			// $('#phone2').val(data.tel);
-			// $('#L_email2').val(data.email);
-			// $('#L_pass2').val(data.password);
-			// $('#L_repass2').val(data.password);
-			// console.log($('#phone2').val());
-			// console.log(data.username);
-
-			// console.log($('#phone2'));
 		}
 	});
 })
-
-function senddata() {
-	return datalist
-}
